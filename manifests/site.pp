d094020dd5e1ba0315115c6a06dbc97f7635a05b
Bibek <bibek20016@users.noreply.github.com>
2016-06-30 11:49:13 -0700

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -43,7 +43,6 @@ include users
 include skeleton
 include memcached
 include nginx
-  nginx::default { 'bibek20016.puppetlabs.vm': }
 include users::admins
   # This is where you can declare classes for all nodes.
   # Example:
